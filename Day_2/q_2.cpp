694805c904257daa8589e7f7a4d4cf68050f51ca
priyanshu mavi <priyanshumavi5@gmail.com>
2026-06-13 18:53:57 +0530

    git config --global user.email "priyanshumavi5@gmail.com"   git config --global user.name "Priyanshu Mavi"

---
+++ Day_2/q_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter a number to reverse: ";
+    cin>>n;
+    int reverse = 0;
+    int temp=n;
+    while(temp!= 0) {
+        int r = temp%10;
+        reverse = (reverse*10)+r;
+        temp /= 10;}
+    cout<<"Reversed number: "<<reverse<< endl;
+}
---
+++ Day_2/q_4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter an integer: ";
+    cin>>n;
+    if(n<0) {
+        cout<<n << " is not a palindrome number."<<endl;
+    }
+    int rev=0;
+    int temp = n;
+    while(temp>0) {
+        int digit=temp%10;
+        rev=(rev*10)+digit;
+        temp=temp/10;}
+    if(n==rev) {
+        cout<<n<< " is a palindrome number."<<endl;
+    } else{
+        cout<<n<<" is not a palindrome number." << endl;
+}
+}
